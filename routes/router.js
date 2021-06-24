5b2158172695907705e3fb9854422ec785870af5
suti1979 <suti1979@gmail.com>
2021-06-24 17:16:39 +0200

    got the chart data :) need to organise

--- routes/router.js
+++ routes/router.js
@@ -4,7 +4,20 @@ const Data = require("./../models/data")
 
 router.get("/", async (req, res) => {
   const all = await Data.find().sort({ date: "desc" })
-  res.render("index", { datas: all })
+  res.render("index", { datas: all, order: convertChartData(all) })
 })
+
+//todo move to a separate file, when working! ;)
+function convertChartData(rows) {
+  const day = rows.map((row) => row.date.getDay())
+  const start = rows.map((row) => row.date.getHours())
+  const time = rows.map((row) => row.time)
+
+  return {
+    x: day,
+    y: start,
+    r: time,
+  }
+}
 
 module.exports = router
