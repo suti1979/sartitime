f2b00ad816cbf2959201a9f0e3cb4c0391aebcdc
suti1979 <suti1979@gmail.com>
2021-06-29 11:08:24 +0200

    some bootstrap

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,21 @@ changeToHourMinute.forEach((e) => {
   e.innerText = convertMilisToHrsMins(e.innerText)
 })
 
+const day = [
+  "vasárnap",
+  "hétfő",
+  "kedd",
+  "szerda",
+  "csütörtök",
+  "péntek",
+  "szombat",
+]
+
+const changeDay = document.querySelectorAll("#day")
+changeDay.forEach((e) => {
+  e.innerText = day[e.innerText]
+})
+
 // <% friday.forEach(f => {%>
 //   <div><%= f.date.getDay() %> <span id="milis"><%= f.time %></span></div>
 //   <%})%>
