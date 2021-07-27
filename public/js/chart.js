d405e1dcc57f537fd0f6c9c7dce2b2e59c8879e3
suti1979 <suti1979@gmail.com>
2021-07-27 13:18:11 +0200

    api fix, chart api read

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,29 +1,35 @@
-const data = {
-  //xLabels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "Travel time / departure time",
-      data: [
-        { x: 0, y: 22, r: 4.4112 },
-        { x: 1, y: 19, r: 4.3836 },
-        { x: 2, y: 19, r: 4.4043 },
-        { x: 3, y: 18, r: 4.2586 },
-        { x: 5, y: 20, r: 4.0635 },
-        { x: 4, y: 5, r: 4.3819 },
-        { x: 6, y: 17, r: 4.4302 },
-      ],
-      backgroundColor: "blue",
-    },
-  ],
-}
+fetch("./sartitime/api_stat")
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    chartDraw(data)
+  })
+  .catch(function (err) {
+    console.warn("Something went wrong.", err)
+  })
 
-const config = {
-  type: "bubble",
-  data: data,
-  options: {},
-}
+function chartDraw(datas) {
+  console.log(datas)
+  const data = {
+    yLabels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Travel time / departure time",
+        data: datas,
+        backgroundColor: "blue",
+      },
+    ],
+  }
 
-const chart = new Chart(document.getElementById("chart"), config)
+  const config = {
+    type: "bubble",
+    data: data,
+    options: {},
+  }
+
+  const chart = new Chart(document.getElementById("chart"), config)
+}
 
 // module.exports = {
 //   actions: [],
--- routes/router.js
+++ routes/router.js
@@ -3,13 +3,27 @@ const router = express.Router()
 const Data = require("./../models/data")
 
 router.get("/", async (req, res) => {
-  const all = await Data.find().sort({ date: "desc" })
-  res.render("index", { datas: all })
+  //TODO samples number missing
+  const lastCheck = await Data.findOne().sort({ date: "desc" })
+  res.render("index", { lastCheck: lastCheck })
 })
 
 router.get("/api", async (req, res) => {
   const all = await Data.find().sort({ date: "desc" })
-  res.send({ datas: all })
+  res.json(all)
+})
+
+router.get("/api_stat", async (req, res) => {
+  const datas = [
+    { x: 0, y: 22, r: 4.4112 },
+    { x: 1, y: 19, r: 12.3836 },
+    { x: 2, y: 11, r: 6.4043 },
+    { x: 3, y: 18, r: 4.2586 },
+    { x: 5, y: 13, r: 7.0635 },
+    { x: 4, y: 8, r: 4.3819 },
+    { x: 6, y: 17, r: 9.4302 },
+  ]
+  res.json(datas)
 })
 
 module.exports = router
